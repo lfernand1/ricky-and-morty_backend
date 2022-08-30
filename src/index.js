d3ed51a503cf054b40e7e8baffb4220a60875ab3
Larissa Fernanda <larifernand7@gmail.com>
2022-08-30 20:51:57 +0044

    a

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const connectDatabase = require("./database/database");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+connectDatabase();
+
 app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.send({ message: "Hello, world!" });
-});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
